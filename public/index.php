4c9cc02e674b8e1a802142c7c5216dd64a55bb42
Skinal <alain.minh.nguyen@gmail.com>
2022-10-11 08:31:31 +0200

    update

--- public/index.php
+++ public/index.php
@@ -3,8 +3,13 @@
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\Factory\AppFactory;
+use App\UserController;
 
 require __DIR__ . '/../vendor/autoload.php';
+
+$container = require_once __DIR__ . '/../bootstrap.php';
+
+AppFactory::setContainer($container);
 
 $app = AppFactory::create();
 
@@ -12,5 +17,28 @@ $app->get('/', function (Request $request, Response $response, $args) {
     $response->getBody()->write("Hello world!");
     return $response;
 });
+
+$app->get('/articles', function (Request $request, Response $response, $args) {
+    $response->getBody()->write("articles");
+    return $response;
+});
+
+$app->get('/articles/{genre}', function (Request $request, Response $response, $args) {
+    $response->getBody()->write("articles du genre ".$args["genre"]);
+    return $response;
+});
+
+$app->get('/articles/{annee:[0-9]{4}}/{mois:[0-9]{2}}', function (Request $request, Response $response, array $args) {
+    $response->getBody()->write(var_dump($args));
+    return $response;
+});
+
+$app->get('/somme[/{params:(?:[0-9]+\/)+}]', function (Request $request,Response $response, $args) {
+    $response->getBody()->write("test");
+    print_r($args);
+    return $response;
+});
+
+$app->get('/users',\App\UserController::class . ':test');
 
 $app->run();
---
+++ src/UserController.php
@@ -0,0 +1,24 @@
+<?php
+namespace App;
+
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use App\UserService;
+
+class UserController{
+    public function __construct(UserService $userService)
+    {
+        $this->userService = $userService;
+    }
+
+    public function test(ServerRequestInterface $request, ResponseInterface $response, array $args): ResponseInterface{
+        $user = $this->userService->signUp('test');
+        $payload = json_encode($user);
+
+        $response->getBody()->write($payload);
+        return $response
+            ->withHeader('Content-Type', 'application/json');
+    }
+
+
+}
